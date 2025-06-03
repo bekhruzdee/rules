20a188bc2d1d7b5b92a6d98e2a670f76d2c2f7d0
bekhruzdee <qutbidinovbehruz003@gmail.com>
2025-06-03 14:35:13 +0500

    add role to tasks.controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,35 +7,43 @@ import {
   Patch,
   Delete,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { RolesGuard } from 'src/auth/role.guard';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
+  @UseGuards(AuthGuard, RolesGuard)
   @Post()
   create(@Body() dto: CreateTaskDto) {
     return this.tasksService.create(dto);
   }
 
+  @UseGuards(AuthGuard)
   @Get()
   findAll() {
     return this.tasksService.findAll();
   }
 
+  @UseGuards(AuthGuard)
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.tasksService.findOne(id);
   }
 
+  @UseGuards(AuthGuard, RolesGuard)
   @Patch(':id')
   update(@Param('id', ParseIntPipe) id: number, @Body() dto: UpdateTaskDto) {
     return this.tasksService.update(id, dto);
   }
 
+  @UseGuards(AuthGuard, RolesGuard)
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.tasksService.remove(id);
