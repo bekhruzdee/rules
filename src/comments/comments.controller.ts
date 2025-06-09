1764882b80bcb6a58fba238e3a619f1808227e36
bekhruzdee <qutbidinovbehruz003@gmail.com>
2025-06-09 10:47:34 +0500

    updated comment.controller.ts

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -6,35 +6,42 @@ import {
   Param,
   Patch,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('comments')
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
 
+  @UseGuards(AuthGuard)
   @Post()
   create(@Body() dto: CreateCommentDto) {
     return this.commentsService.create(dto);
   }
 
+  @UseGuards(AuthGuard)
   @Get('/task/:taskId')
   getByTask(@Param('taskId') taskId: number) {
     return this.commentsService.findByTask(taskId);
   }
 
+  @UseGuards(AuthGuard)
   @Get('/project/:projectId')
   getByProject(@Param('projectId') projectId: number) {
     return this.commentsService.findByProject(projectId);
   }
 
+  @UseGuards(AuthGuard)
   @Patch(':id')
   update(@Param('id') id: number, @Body() dto: UpdateCommentDto) {
     return this.commentsService.update(id, dto);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   remove(@Param('id') id: number) {
     return this.commentsService.remove(id);
